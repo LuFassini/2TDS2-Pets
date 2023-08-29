9208c121ecc61889d16a7a126deefe77fc17d9bc
Luana Fassini <luanafassini.564@gmail.com>
2023-08-29 13:48:18 -0300

    feat: edit functions again

--- assets/js/script.pet.js
+++ assets/js/script.pet.js
@@ -1,123 +1,161 @@
 
 console.log("O JS está linkado!");
 
-    class Pet {
-    constructor(tutor, nomePet,especie, data, foto) {
+function verificarInputs() {
+
+    let tutor = document.getElementById("input-tutor").value;
+    let nomePet = document.getElementById("input-nomePet").value;
+    let especie = document.getElementById("input-especie").value;
+    let foto = document.getElementById("input-foto").value;
+    let data = document.getElementById("input-data").value;
+
+
+    console.log("Titulo" + titulo);
+    console.log("Nome do pet:" + nomePet);
+    console.log("Espécie:" + especie);
+    console.log("Foto:" + foto);
+    console.log("Data:" + data);
+
+
+    if (tutor == "" || nomePet == "" || especie == "" || foto == "" || data == "") {
+
+        console.log("Os dados estao vazios");
+
+        return true;
+    } else {
+        console.log("Os dados nao estao em branco");
+        return false;
+    }
+}
+
+
+class Pet {
+    constructor(tutor, nomePet, especie, data, foto) {
         this.tutor = tutor;
         this.nomePet = nomePet;
-        this.especie = this.especie;
-        this.data = this.data;
-        this.foto = this.foto;
-        this.age = this.calculateAge();
+        this.especie = especie;
+        this.data = data;
+        this.foto = foto;
+        this.age = this.calculateAge(data);
         console.log("Passou pelo constructor da class Pet");
     }
- }
-     function cadastrarPet() {
-    const tutor = document.getElementById("tutor").value;
-    const nomePet = document.getElementById("nomePet").value;
-    const especie = document.getElementById("especie").value;
-    const foto = document.getElementById("foto").value;
-    const data = document.getElementById("data").value;
 
-    petsList.adicionarPet(tutor, nomePet,especie, foto,data);
+    calculateAge(birthdate) {
+        let today = new Date();
+        let birthdate_ = new Date(birthdate);
+        let age = today.getFullYear() - birthdate_.getFullYear();
+        let month = today.getMonth() - birthdate_.getMonth();
+
+        if (month < 0 || (month === 0 && today.getDate() < birthdate_.getDate())) {
+            age--;
+        }
+        return age;
+    
+    }
+    }
+
+function cadastrarPet() {
+    const tutor = document.getElementById("input-tutor").value;
+    const nomePet = document.getElementById("input-nomePet").value;
+    const especie = document.getElementById("input-especie").value;
+    const foto = document.getElementById("input-foto").value;
+    const data = document.getElementById("input-data").value;
+    console.log(tutor);
+    console.log(nomePet);
+    console.log(especie);
+    console.log(foto);
+    console.log(data);
+
+    petsList.adicionarPet(tutor, nomePet, especie, foto, data);
 
     exibirPets();
 }
-    class PetsList {
-        constructor() {
-            this.pets = [];
-        }
-    
-        adicionarPet(tutor, nomePet, especie, foto, data) {
-            if (isAnyInputEmpty()) {
-                sendMSG("Preencha todos os campos!", "error"); 
-            }else if(!isURLValida(imagem)){
-                sendMSG("URL da imagem inválida!", "error");
-            }
-            else{
-                const pet = new Pet(tutor, nomePet, especie, foto, data);
-                this.pets.push(pet);
-                sendMSG("Pet adicionado ao sistema!", "success");
-                clearInputs();
-            }
-    
-        }
-        
-            calculateAge () {
-            let today = new Date();
-            let birthdate = new Date(this.birthdate);
-            let age = today.getFullYear() - birthdate.getFullYear();
-            let month = today.getMonth() - birthdate.getMonth();
-    
-            if (month < 0 || (month === 0 && today.getDate() < birthdate.getDate())) {
-                age--;
-            }
-            console.log("Passou pelo calculateAge() da class User");
-            return age;
-            
-        }
+class PetsList {
+    constructor() {
+        this.pets = [];
     }
-        function exibirPets() {
-        const petList = document.getElementById("petList");
-        petList.innerHTML = "";
+
+    adicionarPet(tutor, nomePet, especie, foto, data) {
+        if (isAnyInputEmpty()) {
+            sendMSG("Preencha todos os campos!", "error");
+        } else if (!isURLValida(foto)) {
+            sendMSG("URL da imagem inválida!", "error");
+        }
+        else {
+            const pet = new Pet(tutor, nomePet, especie, foto, data);
+            this.pets.push(pet);
+            sendMSG("Pet adicionado ao sistema!", "success");
+            clearInputs();
+        }
+
+    }
+
     
-        petsList.pets.forEach(pet => {
-            const cardDiv = `
+}
+function exibirPets() {
+    const petList = document.getElementById("petList");
+    petList.innerHTML = "";
+
+    petsList.pets.forEach(pet => {
+        const cardDiv = `
                 <div class="card">
                     <img src="${pet.foto}" alt="${pet.nomePet}">
-                    <h2>${pet.nomePet}</h2>
-                    <p>Tutor: R$${pet.tutor}</p>
+                    <h2>Nome:${pet.nomePet}</h2>
+                    <p>Tutor:${pet.tutor}</p>
                     <p>Espécie: ${pet.especie}</p>
                     <p>Data de Nascimento: ${pet.data}</p>
+                    <p>Idade: ${pet.age}</p>
                 </div>
             `;
-    
-            petList.innerHTML += cardDiv;
-        });
-    }
-    
-    const petsList = new PetsList();
 
-    function isURLValida(url) {
-        if(url.match(/\.(jpeg|jpg|gif|png)$/) != null){
-            return true;
-        } else {
-            return false;
-        }
+        petList.innerHTML += cardDiv;
+    });
+}
+
+const petsList = new PetsList();
+
+function isURLValida(url) {
+    if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+        return true;
+    } else {
+        return false;
     }
-    function clearInputs(){
-        
-        document.getElementById("tutor").value = "";
-        document.getElementById("nomePet").value = "";
-        document.getElementById("especie").value = "";
-        document.getElementById("foto").value = "";
-        document.getElementById("data").value = "";
-    }
-    function sendMSG(msg,type){  
-    
-        const msgDiv = document.getElementById("msg");
-        msgDiv.innerHTML = "";
-    
-        const msgP = `
+}
+function clearInputs() {
+
+    document.getElementById("input-tutor").value = '';
+    document.getElementById("input-nomePet").value = '';
+    document.getElementById("input-especie").value = '';
+    document.getElementById("input-foto").value = '';
+    document.getElementById("input-data").value = '';
+}
+function sendMSG(msg, type) {
+
+    const msgDiv = document.getElementById("msg");
+    msgDiv.innerHTML = "";
+
+    const msgP = `
             <p class="${type}">${msg}</p>
         `;
+
+    msgDiv.innerHTML += msgP;
+
+    setTimeout(function () {
+        msgDiv.innerHTML = "";
+    }, 3000);
+}
+function isAnyInputEmpty() {
+    const tutor = document.getElementById("input-tutor").value;
+    const nomePet = document.getElementById("input-nomePet").value;
+    const especie = document.getElementById("input-especie").value;
+    const foto = document.getElementById("input-foto").value;
+    const data = document.getElementById("input-data").value;
+
     
-        msgDiv.innerHTML += msgP;
-    
-        setTimeout(function(){
-            msgDiv.innerHTML = "";
-        }, 3000);
+
+    if (tutor == "" || nomePet == "" || especie == "" || foto == "" || data == "") {
+        return true;
+    } else {
+        return false;
     }
-    function isAnyInputEmpty(){
-        const tutor = document.getElementById("tutor").value;
-        const nomePet = document.getElementById("nomePet").value;
-        const especie = document.getElementById("especie").value;
-        const foto = document.getElementById("foto").value;
-        const data = document.getElementById("data").value;
-    
-        if(tutor == "" || nomePet == "" || especie == "" || foto == "" || data == ""){
-            return true;
-        } else {
-            return false;
-        }
-    }
+}
