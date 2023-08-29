b003f2a70444a5454cd72f25f214da3ba4721f0d
Luana Fassini <luanafassini.564@gmail.com>
2023-08-29 08:20:12 -0300

    feat: create function verificarInputs

--- assets/js/script.pet.js
+++ assets/js/script.pet.js
@@ -1,2 +1,44 @@
 // Primeira coisa que eu vou verificar é se meu JS está devidamente linkado no meu HTML.
 console.log("O JS está linkado!");
+
+// Entao para fazer acontecer algo eu preciso de uma funcao
+// Vamos criar uma funcao para vericar se os inputs estao funcionando.
+// Para isso, vamos criar uma funcao chamada verificarInputs.
+// Tudo que esta dentro de uma funcao só executa quando a funcao/acao é chamada.
+
+// Entao para fazer acontecer algo eu preciso de uma funcao
+// Vamos criar uma funcao para vericar se os inputs estao funcionando.
+// Para isso, vamos criar uma funcao chamada verificarInputs.
+// Tudo que esta dentro de uma funcao só executa quando a funcao/acao é chamada.
+function verificarInputs() {
+    // Vamos verificar se os inputs estao colecionando os dados corretamente.
+    // Para isso, vamos criar uma variável para cada input.
+    let tutor = document.getElementById("input-tutor").value;
+    let nomedoPet = document.getElementById("input-nomePet").value;
+    let especie = document.getElementById("input-especie").value;
+    let foto = document.getElementById("input-foto").value;
+    let data = document.getElementById("input-data").value;
+
+    // Vamos dar um console ou verbo consolar para cada variável.
+    console.log("Tutor:" + tutor);
+    console.log("Nome do Pet:" + nomedoPet);
+    console.log("Espécie:" + especie);
+    console.log("Foto:" +  foto);
+    
+
+    // Vamos entao criar uma verificacao se os inputs estao vazios.
+    // Pois se estao vazio eu preciso de um alerta para o usuario.
+    // ou uma mensagem de erro.
+    // Para isso, vamos criar uma condicional if.
+    if (tutor == "" || nomedoPet == "" || especie == "" || foto == "") {
+        // Verificar se o if esta funcionando
+        console.log("Os dados estao vazios");
+        // Como esse é o primeiro if de verificacao, vamos criar 
+        // uma funcao que envia/coloca na tela a mensagem de erro.
+        // Usando a div que já temos com id msg.
+        return true;
+    } else {
+        console.log("Os dados nao estao em branco");
+        return false;
+    }
+}
